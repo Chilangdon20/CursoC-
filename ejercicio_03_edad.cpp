d8ac7f611ecf3349d449c8580375ca0bc1ccefbf
Chilangdon <blocklnoize@gmail.com>
2021-05-24 17:56:36 -0500

    nuevo ejerccio

---
+++ ejercicio_03_edad.cpp
@@ -0,0 +1,14 @@
+
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+    int edad;
+    cout<<"Ingresa tu edad: ";cin>>edad;
+    if((edad>=18) && (edad<=25)){cout<<"\nCumples con la edad";}
+    else{cout<<"\nNO cumples con la edad";}
+    return 0;
+}
